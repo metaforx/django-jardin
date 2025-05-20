4771d39859b9a5989286865413f311dc43e156f3
Marc Widmer <marc@pbi.io>
2025-05-20 21:29:25 +0200

    Refactor SensorData schema usage in API

--- app/jardin_data/api.py
+++ app/jardin_data/api.py
@@ -4,19 +4,13 @@ from .models import SensorData
 
 api = NinjaAPI()
 
-class CreateSensorDataSchema(ModelSchema):
+class SensorDataSchema(ModelSchema):
     class Meta:
         model = SensorData
         exclude = ['created_at']
 
 
-class SensorDataSchema(ModelSchema):
-    class Meta:
-        model = SensorData
-        fields = '__all__'
-
-
 @api.post("/sensor-data", response=SensorDataSchema)
-def create_sensor_data(request, payload: CreateSensorDataSchema):
+def create_sensor_data(request, payload: SensorDataSchema):
     sensor_data = SensorData.objects.create(**payload.dict())
     return sensor_data
